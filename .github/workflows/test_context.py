19c9f0267e2ebb37930cfce7d20de02b06175f86
Arthit Suriyawongkul <arthit@gmail.com>
2024-09-25 05:53:58 +0700

    Add return to testLocalContext
    testLocalContext needs a return of Graph
    
    Also few code formatting.
    
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- .github/workflows/test_context.py
+++ .github/workflows/test_context.py
@@ -1,17 +1,19 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright 2023 spdx contributors
- 
+# SPDX-FileCopyrightText: Copyright 2023 The SPDX Contributors
+
 import sys
 from rdflib import Graph
 
+
 def parseString(data: str) -> Graph:
-  return Graph().parse(data=data, format='json-ld')
+    return Graph().parse(data=data, format="json-ld")
+
 
 def testLocalContext(context_file: str) -> Graph:
-  data = "{ \"@context\": [ \"" + context_file + "\" ], \"@graph\": [ ] }"
-  parseString(data)
+    data = '{ "@context": [ "' + context_file + '" ], "@graph": [ ] }'
+    return parseString(data)
 
 
-if __name__ == '__main__':
-  # run testLocalContext with first argument as context file
-  testLocalContext(sys.argv[1])
+if __name__ == "__main__":
+    # run testLocalContext with first argument as context file
+    testLocalContext(sys.argv[1])
